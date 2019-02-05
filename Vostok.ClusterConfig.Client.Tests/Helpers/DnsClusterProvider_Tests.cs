d3b4b4172a754c78ab9883ee0d08779206b0047d
iloktionov <iloktionov@skbkontur.ru>
2019-02-05 20:10:13 +0500

    + ReplayObservable, a test for DnsClusterProvider

---
+++ Vostok.ClusterConfig.Client.Tests/Helpers/DnsClusterProvider_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.ClusterConfig.Client.Helpers;
+
+namespace Vostok.ClusterConfig.Client.Tests.Helpers
+{
+    [TestFixture]
+    internal class DnsClusterProvider_Tests
+    {
+        [Test]
+        public void Should_return_an_empty_cluster_when_given_dns_record_does_not_exist()
+        {
+            var provider = new DnsClusterProvider(Guid.NewGuid().ToString(), ClusterConfigClientDefaults.Port);
+
+            var cluster = provider.GetCluster();
+
+            cluster.Should().BeEmpty();
+        }
+    }
+}
