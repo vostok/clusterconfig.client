3f1882ff66a7d5b4611cee8697d27ae9f495d276
iloktionov <iloktionov@skbkontur.ru>
2019-02-06 01:14:47 +0500

    ClusterConfigClient: create local updater.

---
+++ Vostok.ClusterConfig.Client/Helpers/FolderLocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using JetBrains.Annotations;
+
+namespace Vostok.ClusterConfig.Client.Helpers
+{
+    internal static class FolderLocator
+    {
+        [CanBeNull]
+        public static DirectoryInfo Locate(string relativePath, int maxOutwardHops)
+        {
+            var baseDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+
+            var initialGuess = new DirectoryInfo(Path.Combine(baseDirectory.FullName, relativePath));
+            if (initialGuess.Exists)
+                return initialGuess;
+
+            for (var i = 0; i < maxOutwardHops; i++)
+            {
+                baseDirectory = baseDirectory.Parent;
+
+                if (baseDirectory == null)
+                    break;
+
+                var folder = new DirectoryInfo(Path.Combine(baseDirectory.FullName, relativePath));
+                if (folder.Exists)
+                    return folder;
+            }
+
+            return initialGuess;
+        }
+    }
+}
