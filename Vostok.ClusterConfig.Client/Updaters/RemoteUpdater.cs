6790140cefa8e8e05489de5f825f5e3b14a46ff1
iloktionov <iloktionov@skbkontur.ru>
2019-02-06 02:12:28 +0500

    Continue implementing RemoteUpdater.

--- Vostok.ClusterConfig.Client/Updaters/RemoteUpdater.cs
+++ Vostok.ClusterConfig.Client/Updaters/RemoteUpdater.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Vostok.Clusterclient.Core;
 using Vostok.Clusterclient.Core.Misc;
+using Vostok.Clusterclient.Core.Model;
 using Vostok.Clusterclient.Core.Ordering.Weighed;
 using Vostok.Clusterclient.Core.Strategies;
 using Vostok.Clusterclient.Core.Topology;
@@ -17,10 +18,12 @@ namespace Vostok.ClusterConfig.Client.Updaters
 
         private readonly IClusterClient client;
         private readonly ILog log;
+        private readonly string zone;
 
-        public RemoteUpdater(IClusterProvider clusterProvider, ILog log)
+        public RemoteUpdater(IClusterProvider clusterProvider, ILog log, string zone)
         {
             this.log = log;
+            this.zone = zone;
 
             client = new ClusterClient(
                 log.WithMinimumLevel(LogLevel.Warn),
@@ -38,7 +41,7 @@ namespace Vostok.ClusterConfig.Client.Updaters
                         LogResultDetails = false
                     };
 
-                    config.MaxReplicasUsedPerRequest = 3;
+                    config.MaxReplicasUsedPerRequest = int.MaxValue;
 
                     config.TargetServiceName = TargetService;
 
@@ -47,13 +50,46 @@ namespace Vostok.ClusterConfig.Client.Updaters
 
                     // TODO(iloktionov): auxiliary headers (client identity, request timeout)
 
-                    // TODO(iloktionov): custom response criteria (reject 400s?)
+                    // TODO(iloktionov): custom response criteria (reject 404s?)
                 });
         }
 
+        // TODO(iloktionov): handle disabled case
+
+        // TODO(iloktionov): handle the case when there are no replicas and there was no previous state (assume there's no CC deployed)
+
+        // TODO(iloktionov): do not trust 404 response from a single replica
+
+        // TODO(iloktionov): do not trust 304 response if a version has not been sent
+
+        // TODO(iloktionov): protect against flapping between two versions when communicating with out-of-sync replicas
+
         public async Task<RemoteUpdateResult> UpdateAsync(RemoteUpdateResult lastResult, CancellationToken cancellationToken)
         {
+            var request = Request.Get(zone);
+
+            var result = await client.SendAsync(request, cancellationToken: cancellationToken).ConfigureAwait(false);
+
+            // TODO(iloktionov): handle errors in result
+
+            var response = result.Response;
+
+            // TODO(iloktionov): handle different response codes
+
             throw new NotImplementedException();
+        }
+
+        private Request CreateRequest(RemoteUpdateResult lastResult)
+        {
+            var request = Request.Get(zone)
+                .WithAdditionalQueryParameter("binaryProtocol", "v1")
+                .WithAcceptHeader("application/octet-stream")
+                .WithAcceptEncodingHeader("gzip");
+
+            if (lastResult != null)
+                request = request.WithIfModifiedSinceHeader(lastResult.Version);
+
+            return request;
         }
     }
 }
