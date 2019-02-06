b4fe97edcb36439349391dc4a7e1fa35807be189
iloktionov <iloktionov@skbkontur.ru>
2019-02-07 01:10:50 +0500

    Introduced DefaultSettingsProvider.

---
+++ Vostok.ClusterConfig.Client/DefaultSettingsProvider.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.ClusterConfig.Client
+{
+    /// <summary>
+    /// <para>Provides default settings used in parameterless ctor of <see cref="ClusterConfigClient"/>.</para>
+    /// <para>Looks in an optional configuration file located in <see cref="ClusterConfigClientDefaults.LocalFolder"/> and named <see cref="ClusterConfigClientDefaults.ConfigurationFile"/>.</para>
+    /// </summary>
+    [PublicAPI]
+    public static class DefaultSettingsProvider
+    {
+        [NotNull]
+        public static ClusterConfigClientSettings Settings { get; } = new ClusterConfigClientSettings();
+    }
+}
