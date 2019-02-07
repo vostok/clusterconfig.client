24e574accdfdad2b113f9208214527a8d560f16d
iloktionov <iloktionov@skbkontur.ru>
2019-02-07 18:24:28 +0500

    TreeExtractor: added prefix-based deduplication to avoid wasting memory on existing subtrees.

--- Vostok.ClusterConfig.Client/Helpers/TreeExtractor.cs
+++ Vostok.ClusterConfig.Client/Helpers/TreeExtractor.cs
@@ -1,4 +1,7 @@
-﻿using JetBrains.Annotations;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
 using Vostok.ClusterConfig.Client.Abstractions;
 using Vostok.Configuration.Abstractions.Merging;
 using Vostok.Configuration.Abstractions.SettingsTree;
@@ -20,11 +23,33 @@ namespace Vostok.ClusterConfig.Client.Helpers
                 path,
                 p =>
                 {
+                    foreach (var prefix in EnumeratePrefixes(p))
+                    {
+                        if (state.Cache.TryGetValue(prefix, out var tree))
+                            return tree.ScopeTo(path.Segments.Skip(prefix.Segments.Count()));
+                    }
+
                     var remoteSettings = state.RemoteTree?.GetSettings(p);
                     var localSettings = state.LocalTree?.ScopeTo(p.Segments);
 
                     return SettingsNodeMerger.Merge(remoteSettings, localSettings, MergeOptions);
                 });
+        }
+
+        private static IEnumerable<ClusterConfigPath> EnumeratePrefixes(ClusterConfigPath path)
+        {
+            var segments = path.Segments.ToArray();
+            var builder = new StringBuilder();
+
+            for (var i = 0; i < segments.Length - 1; i++)
+            {
+                if (i > 0)
+                    builder.Append(ClusterConfigPath.Separator);
+
+                builder.Append(segments[i]);
+
+                yield return builder.ToString();
+            }
         }
     }
 }
