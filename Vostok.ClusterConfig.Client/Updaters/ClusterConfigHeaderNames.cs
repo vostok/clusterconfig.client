092f02eaa83faf65555d84cc92a9bc658b24f2aa
a.tolstov <a.tolstov@skbkontur.ru>
2022-01-27 05:18:31 +0500

    refactor: ClusterConfigHeaderNames

--- Vostok.ClusterConfig.Client/Updaters/ClusterConfigHeaderNames.cs
+++ Vostok.ClusterConfig.Client/Updaters/ClusterConfigHeaderNames.cs
@@ -2,6 +2,7 @@ namespace Vostok.ClusterConfig.Client.Updaters
 {
     public static class ClusterConfigHeaderNames
     {
-        public const string RecommendedProtocol = "x-kontur-recommended-protocol";
+        public const string RecommendedProtocol = "X-Kontur-ClusterConfig-RecommendedProtocol";
+        public const string ReplicaName = "X-Kontur-ClusterConfig-Replica";
     }
 }
