ad0a32f181f4c8149c437d2d58f46dedea34c00b
a.tolstov <a.tolstov@skbkontur.ru>
2022-01-28 13:56:30 +0500

    refactor: More documentations

--- Vostok.ClusterConfig.Client/ProtocolVersion.cs
+++ Vostok.ClusterConfig.Client/ProtocolVersion.cs
@@ -1,9 +1,25 @@
-namespace Vostok.ClusterConfig.Client.Abstractions
+using System;
+using JetBrains.Annotations;
+
+namespace Vostok.ClusterConfig.Client
 {
+    /// <summary>
+    /// ClusterConfig communication protocols.
+    /// </summary>
+    [PublicAPI]
     public enum ProtocolVersion
     {
         // V0 = 0, // reserved for legacy protocol
+        
+        /// <summary>
+        /// First binary protocol. Not very effective and now considered obsolete. 
+        /// </summary>
+        [Obsolete("Use V2 instead")]
         V1 = 1,
+        
+        /// <summary>
+        /// Second binary protocol. Very effective: send patches for zone instead full zone.
+        /// </summary>
         V2 = 2
     }
 }
