aa72ddebe2850d4215ad350d3df4d95971f0c763
deniaa <deniaa@skbkontur.ru>
2024-12-20 18:12:38 +0500

    rename test

--- Vostok.ClusterConfig.Client.Tests/Functional/FunctionalTests_V3_RecommendedProtocol.cs
+++ Vostok.ClusterConfig.Client.Tests/Functional/FunctionalTests_V3_RecommendedProtocol.cs
@@ -84,7 +84,7 @@ internal class FunctionalTests_V3_RecommendedProtocol
     [TestCase(ClusterConfigProtocolVersion.V1)]
     [TestCase(ClusterConfigProtocolVersion.V2)]
     [TestCase(ClusterConfigProtocolVersion.V3)]
-    public void T(ClusterConfigProtocolVersion protocol)
+    public void Client_should_be_able_to_downgrade_to_recommended_protocol(ClusterConfigProtocolVersion protocol)
     {
         server.SetResponse(remoteTree1, version1);
 
